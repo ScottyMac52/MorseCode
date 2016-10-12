58f015f87a72a3fa42d05366c865d119597533e5
smcintosh <smcintosh@SCOTTMCINTOSH>
2016-10-11 22:41:13 -0700

    Add project files.

---
+++ MorseCode/MorseCode.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MorseCode.ino
+ Created:	10/11/2016 10:40:48 PM
+ Author:	smcintosh
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
